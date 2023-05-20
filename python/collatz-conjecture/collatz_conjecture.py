d2ca642c331604d2188ce4fb43b9dfd29b9eff3b
Erik Moon <sertvitas@gmail.com>
2023-05-20 07:04:33 -0400

    collatz

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,23 @@
+"""
+Collatz Conjecture exercise.
+"""
 def steps(number):
-    pass
+    """
+
+    :param number: int - starting number for the series
+    :return: int - the number of steps it took to get to 1
+    """
+    # number = int(input('Enter a number: '))
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    sequence = [number]
+    while number != 1:
+        if(number % 2) == 0:
+            number = number // 2
+        else:
+            number = (number * 3) + 1
+        sequence.append(number)
+    print(sequence)
+    tries = len(sequence)-1
+    print(tries)
+    return tries
